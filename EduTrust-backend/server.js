937105ba1fde58afba9f1fc3dde444b650f7f8f5
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-03 23:14:12 +0100

    Add cors to the code

--- EduTrust-backend/server.js
+++ EduTrust-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 
