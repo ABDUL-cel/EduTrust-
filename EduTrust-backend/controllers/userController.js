194b075a6ca3e9d2fdd7c6a78aed3206f47ca256
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-03 22:10:17 +0100

    add my backend

---
+++ EduTrust-backend/controllers/userController.js
@@ -0,0 +1,41 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'Please include all fields' });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        message: 'User registered successfully',
+      });
+    } else {
+      res.status(400).json({ message: 'Invalid user data' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+module.exports = { registerUser };
