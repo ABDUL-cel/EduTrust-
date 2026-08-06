f658fa0a22ea4a4f5bd73aef4197e53b85ff271d
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-06 16:05:08 +0100

    Update and rename Student.js to student.js

---
+++ EduTrust-backend/models/student.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+
+    school_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "School",
+        required: true
+    },
+
+    parent_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Parent",
+        default: null
+    },
+
+    admission_number: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+
+    first_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    last_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    other_name: {
+        type: String,
+        default: ""
+    },
+
+    gender: {
+        type: String,
+        enum: ["Male", "Female"],
+        required: true
+    },
+
+    date_of_birth: {
+        type: Date,
+        required: true
+    },
+
+    class_name: {
+        type: String,
+        required: true
+    },
+
+    arm: {
+        type: String,
+        default: ""
+    },
+
+    admission_date: {
+        type: Date,
+        default: Date.now
+    },
+
+    passport: {
+        type: String,
+        default: ""
+    },
+
+    home_address: {
+        type: String,
+        default: ""
+    },
+
+    medical_information: {
+        type: String,
+        default: ""
+    },
+
+    status: {
+        type: String,
+        enum: [
+            "Pending",
+            "Active",
+            "Suspended",
+            "Graduated",
+            "Archived"
+        ],
+        default: "Pending"
+    }
+
+},
+{
+    timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+});
+
+module.exports = mongoose.model("Student", StudentSchema);
