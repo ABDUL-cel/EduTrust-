e74776a5722fd8db35fdcbe84f1ee6de21bad560
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-07 10:13:06 +0100

    Update dashboardRoutes.js

--- EduTrust-backend/routes/dashboardRoutes.js
+++ EduTrust-backend/routes/dashboardRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
+
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
+
 const {
     getDashboardOverview
 } = require("../controllers/dashboardController");
 
+// =======================================
 // Dashboard Overview
-router.get("/overview", authMiddleware, getDashboardOverview);
+// GET /api/dashboard/overview
+// =======================================
+router.get(
+    "/overview",
+    authMiddleware,
+    getDashboardOverview
+);
 
 module.exports = router;
