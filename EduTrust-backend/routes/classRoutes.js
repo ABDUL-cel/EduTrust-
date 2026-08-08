4d2bb98c26d53f55d4f3dd149f830ad34030f7da
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-08 15:21:36 +0100

    Update classRoutes.js

--- EduTrust-backend/routes/classRoutes.js
+++ EduTrust-backend/routes/classRoutes.js
@@ -9,32 +9,88 @@ const {
     createClass,
     getClasses,
     updateClass,
-    archiveClass
+    archiveClass,
+    getMyClasses,
+    getMyClass
 } = require("../controllers/classController");
+
 
 router.use(authMiddleware);
 
+
+// =======================================
+// Teacher's Assigned Classes
+// =======================================
+
+router.get(
+    "/my/classes",
+    roleMiddleware("Teacher"),
+    getMyClasses
+);
+
+
+router.get(
+    "/my/classes/:id",
+    roleMiddleware("Teacher"),
+    getMyClass
+);
+
+
+// =======================================
+// All School Classes
+// =======================================
+
 router.get(
     "/",
+    roleMiddleware(
+        "Principal",
+        "Vice Principal",
+        "Teacher"
+    ),
     getClasses
 );
 
+
+// =======================================
+// Create Class
+// =======================================
+
 router.post(
     "/",
-    roleMiddleware("Principal", "Vice Principal"),
+    roleMiddleware(
+        "Principal",
+        "Vice Principal"
+    ),
     createClass
 );
 
+
+// =======================================
+// Update Class
+// =======================================
+
 router.put(
     "/:id",
-    roleMiddleware("Principal", "Vice Principal"),
+    roleMiddleware(
+        "Principal",
+        "Vice Principal"
+    ),
     updateClass
 );
 
+
+// =======================================
+// Archive Class
+// =======================================
+
 router.patch(
     "/:id/archive",
-    roleMiddleware("Principal", "Vice Principal"),
+    roleMiddleware(
+        "Principal",
+        "Vice Principal"
+    ),
     archiveClass
 );
+
 
 module.exports = router;
