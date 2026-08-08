f000167b90c94cb660764fb246a2d3581d703845
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-08 13:47:15 +0100

    Create subjectRoutes.js

---
+++ EduTrust-backend/routes/subjectRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+
+const {
+    createSubject,
+    getSubjects,
+    updateSubject,
+    archiveSubject
+} = require("../controllers/subjectController");
+
+router.use(authMiddleware);
+
+router.get(
+    "/",
+    getSubjects
+);
+
+router.post(
+    "/",
+    roleMiddleware("Principal", "Vice Principal"),
+    createSubject
+);
+
+router.put(
+    "/:id",
+    roleMiddleware("Principal", "Vice Principal"),
+    updateSubject
+);
+
+router.patch(
+    "/:id/archive",
+    roleMiddleware("Principal", "Vice Principal"),
+    archiveSubject
+);
+
+module.exports = router;
