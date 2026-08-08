d42197b1a4fd1b7b51e9dd7d31a3b900203afbb5
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-08 18:37:39 +0100

    Create notificationRoutes.js

---
+++ EduTrust-backend/routes/notificationRoutes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authMiddleware =
+    require("../middleware/authMiddleware");
+
+const roleMiddleware =
+    require("../middleware/roleMiddleware");
+
+const {
+    getMyNotifications,
+    markAsRead,
+    markAllAsRead,
+    createNotification
+} = require("../controllers/notificationController");
+
+
+router.use(authMiddleware);
+
+
+// =======================================
+// My Notifications
+// =======================================
+router.get(
+    "/",
+    getMyNotifications
+);
+
+
+// =======================================
+// Mark One As Read
+// =======================================
+router.patch(
+    "/:id/read",
+    markAsRead
+);
+
+
+// =======================================
+// Mark All As Read
+// =======================================
+router.patch(
+    "/read-all",
+    markAllAsRead
+);
+
+
+// =======================================
+// Create Notification
+// =======================================
+router.post(
+    "/",
+    roleMiddleware(
+        "Principal",
+        "Vice Principal"
+    ),
+    createNotification
+);
+
+
+module.exports = router;
