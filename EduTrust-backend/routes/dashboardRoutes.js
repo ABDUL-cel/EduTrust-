f71274127d136fd94750111051171fafe1bab4bf
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-19 02:55:52 +0100

    Update dashboardRoutes.js

--- EduTrust-backend/routes/dashboardRoutes.js
+++ EduTrust-backend/routes/dashboardRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authMiddleware");
+// 1. DESTRUCTURE authMiddleware WITH CURLY BRACES
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 const {
     getDashboardOverview
