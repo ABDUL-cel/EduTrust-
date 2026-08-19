7b101613e4d48d22f65309ad57cb095e74dc16ab
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-19 03:14:19 +0100

    Update resultRoutes.js

--- EduTrust-backend/routes/resultRoutes.js
+++ EduTrust-backend/routes/resultRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authMiddleware");
+ const { authMiddleware } = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
