ab0244d9dc44fa8c9719aac06cdcd74e9a34cd1f
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-08-19 03:13:58 +0100

    Update staffRoutes.js

--- EduTrust-backend/routes/staffRoutes.js
+++ EduTrust-backend/routes/staffRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const authMiddleware = require("../middleware/authMiddleware");
+ const { authMiddleware } = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
